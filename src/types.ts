4e1da9827055b8c71de6329a64d3aeedfdf31f00
Erik Sundell <erik.sundell87@gmail.com>
2021-06-03 16:28:42 +0200

    use aws-sdk in frontend

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-import { DataQuery, DataSourceJsonData } from '@grafana/data';
+import { DataQuery } from '@grafana/data';
+import { AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData } from '@grafana/aws-sdk';
 
 export interface RedshiftQuery extends DataQuery {}
 
@@ -7,11 +8,13 @@ export const defaultQuery: Partial<RedshiftQuery> = {};
 /**
  * These are options configured for each DataSource instance
  */
-export interface RedshiftDataSourceOptions extends DataSourceJsonData {
-  path?: string;
+export interface RedshiftDataSourceOptions extends AwsAuthDataSourceJsonData {
+  clusterIdentifier?: string;
+  database?: string;
+  dbUser?: string;
 }
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
-export interface RedshiftDataSourceSecureJsonData {}
+export interface RedshiftDataSourceSecureJsonData extends AwsAuthDataSourceSecureJsonData {}
