7e36df8670a27ad7ab55d4f8b7d76f158d35ebcb
Erik Sundell <erik.sundell87@gmail.com>
2021-06-03 14:38:53 +0200

    cleanup backend

---
+++ pkg/datasource.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+)
+
+var (
+	_ backend.QueryDataHandler      = (*RedshiftDatasource)(nil)
+	_ backend.CheckHealthHandler    = (*RedshiftDatasource)(nil)
+	_ instancemgmt.InstanceDisposer = (*RedshiftDatasource)(nil)
+)
+
+// NewRedshiftDatasource creates a new datasource instance.
+func NewRedshiftDatasource(_ backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	return &RedshiftDatasource{}, nil
+}
+
+// RedshiftDatasource is the redshift data source backend host
+type RedshiftDatasource struct{}
+
+// Dispose cleans up datasource instance resources.
+func (d *RedshiftDatasource) Dispose() {
+}
+
+// QueryData handles multiple queries and returns multiple responses.
+// req contains the queries []DataQuery (where each query contains RefID as a unique identifier).
+// The QueryDataResponse contains a map of RefID to the response for each query, and each response
+// contains Frames ([]*Frame).
+func (d *RedshiftDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	log.DefaultLogger.Info("QueryData called", "request", req)
+
+	// create response struct
+	response := backend.NewQueryDataResponse()
+
+	// loop over queries and execute them individually.
+	for _, q := range req.Queries {
+		res := d.query(ctx, req.PluginContext, q)
+
+		// save the response in a hashmap
+		// based on with RefID as identifier
+		response.Responses[q.RefID] = res
+	}
+
+	return response, nil
+}
+
+type queryModel struct {
+}
+
+func (d *RedshiftDatasource) query(_ context.Context, pCtx backend.PluginContext, query backend.DataQuery) backend.DataResponse {
+	response := backend.DataResponse{}
+
+	// Unmarshal the JSON into our queryModel.
+	var qm queryModel
+
+	response.Error = json.Unmarshal(query.JSON, &qm)
+	if response.Error != nil {
+		return response
+	}
+
+	frame := data.NewFrame("response")
+
+	frame.Fields = append(frame.Fields,
+		data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
+		data.NewField("values", nil, []int64{10, 20}),
+	)
+
+	response.Frames = append(response.Frames, frame)
+
+	return response
+}
+
+// CheckHealth handles health checks sent from Grafana to the plugin.
+// The main use case for these health checks is the test button on the
+// datasource configuration page which allows users to verify that
+// a datasource is working as expected.
+func (d *RedshiftDatasource) CheckHealth(_ context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+	log.DefaultLogger.Info("CheckHealth called", "request", req)
+
+	var status = backend.HealthStatusOk
+	var message = "Data source is working"
+
+	if rand.Int()%2 == 0 {
+		status = backend.HealthStatusError
+		message = "randomized error"
+	}
+
+	return &backend.CheckHealthResult{
+		Status:  status,
+		Message: message,
+	}, nil
+}
