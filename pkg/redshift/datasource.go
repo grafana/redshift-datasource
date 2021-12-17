4f2632c44583d7ff24893f8591fcbc847a3fef95
Andres Martinez Gotor <andres.martinez@grafana.com>
2021-12-17 10:48:02 +0100

    Use AWS sdk packages for models and datasource (#103)

--- pkg/redshift/datasource.go
+++ pkg/redshift/datasource.go
@@ -4,19 +4,16 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"fmt"
 
-	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
 	sqlAPI "github.com/grafana/grafana-aws-sdk/pkg/sql/api"
+	"github.com/grafana/grafana-aws-sdk/pkg/sql/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana-plugin-sdk-go/data/sqlutil"
 	"github.com/grafana/redshift-datasource/pkg/redshift/api"
 	"github.com/grafana/redshift-datasource/pkg/redshift/driver"
 	"github.com/grafana/redshift-datasource/pkg/redshift/models"
 	"github.com/grafana/sqlds/v2"
-	"github.com/pkg/errors"
 )
 
 type RedshiftDatasourceIface interface {
@@ -31,13 +28,11 @@ type RedshiftDatasourceIface interface {
 }
 
 type RedshiftDatasource struct {
-	sessionCache *awsds.SessionCache
+	awsDS *datasource.AWSDatasource
 }
 
 func New() *RedshiftDatasource {
-	return &RedshiftDatasource{
-		sessionCache: awsds.NewSessionCache(),
-	}
+	return &RedshiftDatasource{awsDS: datasource.New()}
 }
 
 func (s *RedshiftDatasource) Settings(_ backend.DataSourceInstanceSettings) sqlds.DriverSettings {
@@ -53,51 +48,24 @@ func (s *RedshiftDatasource) Converters() (sc []sqlutil.Converter) {
 }
 
 // Connect opens a sql.DB connection using datasource settings
-func (s *RedshiftDatasource) Connect(config backend.DataSourceInstanceSettings, _ json.RawMessage) (*sql.DB, error) {
-	settings := models.RedshiftDataSourceSettings{}
-	err := settings.Load(config)
-	if err != nil {
-		return nil, fmt.Errorf("error reading settings: %s", err.Error())
-	}
-
-	api, err := api.New(s.sessionCache, &settings)
+func (s *RedshiftDatasource) Connect(config backend.DataSourceInstanceSettings, queryArgs json.RawMessage) (*sql.DB, error) {
+	s.awsDS.Init(config)
+	args, err := sqlds.ParseOptions(queryArgs)
 	if err != nil {
 		return nil, err
 	}
 
-	dr, err := driver.New(api)
-	if err != nil {
-		return nil, err
-	}
-	db, err := dr.OpenDB()
-	if err != nil {
-		return nil, errors.WithMessage(err, "Failed to connect to database. Is the hostname and port correct?")
-	}
-
-	return db, nil
+	return s.awsDS.GetDB(config.ID, args, models.New, api.New, driver.New)
 }
 
-func (s *RedshiftDatasource) getApi(ctx context.Context) (*api.API, error) {
-	plugin := httpadapter.PluginConfigFromContext(ctx)
-	if plugin.DataSourceInstanceSettings == nil {
-		return nil, fmt.Errorf("unable to get settings from request")
-	}
-
-	settings := models.RedshiftDataSourceSettings{}
-	err := settings.Load(*plugin.DataSourceInstanceSettings)
-	if err != nil {
-		return nil, fmt.Errorf("error reading settings: %s", err.Error())
-	}
-
-	res, err := api.New(s.sessionCache, &settings)
-	if err != nil {
-		return nil, err
-	}
-	return res.(*api.API), nil
+func (s *RedshiftDatasource) getApi(ctx context.Context, options sqlds.Options) (*api.API, error) {
+	id := datasource.GetDatasourceID(ctx)
+	res, err := s.awsDS.GetAPI(id, options, models.New, api.New)
+	return res.(*api.API), err
 }
 
 func (s *RedshiftDatasource) Regions(ctx context.Context) ([]string, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, sqlds.Options{})
 	if err != nil {
 		return nil, err
 	}
@@ -109,7 +77,7 @@ func (s *RedshiftDatasource) Regions(ctx context.Context) ([]string, error) {
 }
 
 func (s *RedshiftDatasource) Databases(ctx context.Context, options sqlds.Options) ([]string, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
@@ -121,7 +89,7 @@ func (s *RedshiftDatasource) Databases(ctx context.Context, options sqlds.Option
 }
 
 func (s *RedshiftDatasource) Schemas(ctx context.Context, options sqlds.Options) ([]string, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
@@ -133,7 +101,7 @@ func (s *RedshiftDatasource) Schemas(ctx context.Context, options sqlds.Options)
 }
 
 func (s *RedshiftDatasource) Tables(ctx context.Context, options sqlds.Options) ([]string, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
@@ -145,7 +113,7 @@ func (s *RedshiftDatasource) Tables(ctx context.Context, options sqlds.Options) 
 }
 
 func (s *RedshiftDatasource) Columns(ctx context.Context, options sqlds.Options) ([]string, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
@@ -157,7 +125,7 @@ func (s *RedshiftDatasource) Columns(ctx context.Context, options sqlds.Options)
 }
 
 func (s *RedshiftDatasource) Secrets(ctx context.Context, options sqlds.Options) ([]models.ManagedSecret, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
@@ -165,7 +133,7 @@ func (s *RedshiftDatasource) Secrets(ctx context.Context, options sqlds.Options)
 }
 
 func (s *RedshiftDatasource) Secret(ctx context.Context, options sqlds.Options) (*models.RedshiftSecret, error) {
-	api, err := s.getApi(ctx)
+	api, err := s.getApi(ctx, options)
 	if err != nil {
 		return nil, err
 	}
