1625aee9086653358daad7194ffc44d31d0f6ce0
Erik Sundell <erik.sundell87@gmail.com>
2022-08-23 10:14:46 +0200

    silently swallow fetch error

--- src/SQLEditor.tsx
+++ src/SQLEditor.tsx
@@ -33,16 +33,18 @@ export default function SQLEditor({ query, datasource, onRunQuery, onChange }: R
   };
 
   const getSchemas = useCallback(async () => {
-    const schemas: string[] = await datasource.postResource('schemas');
+    const schemas: string[] = await datasource.postResource('schemas').catch(() => []);
     return schemas.map((schema) => ({ name: schema, completion: schema }));
   }, [datasource]);
 
   const getTables = useCallback(
     async (schema?: string) => {
-      const tables: string[] = await datasource.postResource('tables', {
-        // if schema is provided in the raw sql use that. if not, use schema defined in the query builder.
-        schema: interpolate(schema) ?? queryRef.current.schema,
-      });
+      const tables: string[] = await datasource
+        .postResource('tables', {
+          // if schema is provided in the raw sql use that. if not, use schema defined in the query builder.
+          schema: interpolate(schema) ?? queryRef.current.schema,
+        })
+        .catch(() => []);
       return tables.map((table) => ({ name: table, completion: table }));
     },
     [datasource]
@@ -50,11 +52,13 @@ export default function SQLEditor({ query, datasource, onRunQuery, onChange }: R
 
   const getColumns = useCallback(
     async (tableName?: string, schema?: string) => {
-      const columns: string[] = await datasource.postResource('columns', {
-        // if schema and table have been provided in the raw sql use that. if not, use schema/table defined in the query builder.
-        schema: interpolate(schema) ?? queryRef.current.schema,
-        table: interpolate(tableName) ?? queryRef.current.table,
-      });
+      const columns: string[] = await datasource
+        .postResource('columns', {
+          // if schema and table have been provided in the raw sql use that. if not, use schema/table defined in the query builder.
+          schema: interpolate(schema) ?? queryRef.current.schema,
+          table: interpolate(tableName) ?? queryRef.current.table,
+        })
+        .catch(() => []);
       return columns.map((column) => ({ name: column, completion: column }));
     },
     [datasource]
