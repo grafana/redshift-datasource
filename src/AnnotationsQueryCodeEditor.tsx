1316bfc9510da43693bd9aaa5220d755e8e9a16a
Ida Štambuk <ida.stambuk@grafana.com>
2023-02-01 11:02:51 +0100

    Remove run and cancel buttons in annotations editor (#206)

---
+++ src/AnnotationsQueryCodeEditor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { RedshiftQuery, RedshiftDataSourceOptions } from './types';
+import { QueryEditorProps } from '@grafana/data';
+import { DataSource } from 'datasource';
+import { QueryEditor } from 'QueryEditor';
+
+export function AnnotationsQueryCodeEditor(
+  props: QueryEditorProps<DataSource, RedshiftQuery, RedshiftDataSourceOptions>
+) {
+  return <QueryEditor {...props} hideRunQueryButtons></QueryEditor>;
+}
