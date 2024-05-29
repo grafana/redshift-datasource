93336afd3fe6d59c58752d28fa8eff9482f85788
Ida Štambuk <ida.stambuk@grafana.com>
2024-05-29 17:38:06 +0200

    increase timeout?

--- cypress/integration/smoke.spec.ts
+++ cypress/integration/smoke.spec.ts
@@ -58,7 +58,7 @@ e2e.scenario({
             e2eSelectors.ConfigEditor.ManagedSecret.input().click({ force: true });
             e2eSelectors.ConfigEditor.ManagedSecret.input().type(datasource.jsonData.managedSecret.name);
             // wait for it to load
-            e2eSelectors.ConfigEditor.ManagedSecret.testID().contains(datasource.jsonData.managedSecret.name);
+            e2eSelectors.ConfigEditor.ManagedSecret.testID().contains(datasource.jsonData.managedSecret.name, {timeout: 10000});
             e2eSelectors.ConfigEditor.ManagedSecret.input().type('{enter}');
             // wait for the secret to be retrieved
             e2eSelectors.ConfigEditor.ClusterIDText.testID().should(
