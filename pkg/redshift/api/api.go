7acc6a19051a19c2cffbc596eacbf3f691d480d9
idastambuk <ida.stambuk@grafana.com>
2024-08-08 12:58:46 +0200

    Update aws-sdk and don't fetch regions from BE

--- pkg/redshift/api/api.go
+++ pkg/redshift/api/api.go
@@ -171,40 +171,8 @@ func (c *API) Stop(output *api.ExecuteQueryOutput) error {
 }
 
 func (c *API) Regions(aws.Context) ([]string, error) {
-	// List from https://docs.aws.amazon.com/general/latest/gr/redshift-service.html
-	return []string{
-		"us-east-2",
-		"us-east-1",
-		"us-west-1",
-		"us-west-2",
-		"af-south-1",
-		"ap-east-1",
-		"ap-south-2",
-		"ap-southeast-3",
-		"ap-southeast-4",
-		"ap-south-1",
-		"ap-northeast-3",
-		"ap-northeast-2",
-		"ap-southeast-1",
-		"ap-southeast-2",
-		"ap-northeast-1",
-		"ca-central-1",
-		"ca-west-1",
-		"eu-central-1",
-		"eu-west-1",
-		"eu-west-2",
-		"eu-south-1",
-		"eu-west-3",
-		"eu-south-2",
-		"eu-north-1",
-		"eu-central-2",
-		"il-central-1",
-		"me-south-1",
-		"me-central-1",
-		"sa-east-1",
-		"us-gov-east-1",
-		"us-gov-west-1",
-	}, nil
+	// This is not used. If regions are out of date, update them in the @grafana/aws-sdk-react package
+	return []string{}, nil
 }
 
 func (c *API) Databases(ctx aws.Context, options sqlds.Options) ([]string, error) {
