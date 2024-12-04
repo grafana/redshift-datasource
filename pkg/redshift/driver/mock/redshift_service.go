942c4b7e24e4b6725a49c730a61ed85abf8b1faf
Nathan Verzemnieks <njvrzm@gmail.com>
2024-12-04 16:41:18 +0100

    clean up import names (no need for "v2")

--- pkg/redshift/driver/mock/redshift_service.go
+++ pkg/redshift/driver/mock/redshift_service.go
@@ -3,14 +3,14 @@ package redshiftservicemock
 import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	redshiftdataV2 "github.com/aws/aws-sdk-go-v2/service/redshiftdata"
-	redshiftdataV2types "github.com/aws/aws-sdk-go-v2/service/redshiftdata/types"
+	"github.com/aws/aws-sdk-go-v2/service/redshiftdata"
+	redshiftdatatypes "github.com/aws/aws-sdk-go-v2/service/redshiftdata/types"
 )
 
 const SinglePageResponseQueryId = "singlePageResponse"
 const MultiPageResponseQueryId = "multiPageResponse"
 
-var columnMetaData = []redshiftdataV2types.ColumnMetadata{
+var columnMetaData = []redshiftdatatypes.ColumnMetadata{
 	{
 		Name:     aws.String("col1"),
 		Nullable: 1,
@@ -23,14 +23,14 @@ var columnMetaData = []redshiftdataV2types.ColumnMetadata{
 	},
 }
 
-var twoRecords = [][]redshiftdataV2types.Field{
+var twoRecords = [][]redshiftdatatypes.Field{
 	{
-		&redshiftdataV2types.FieldMemberStringValue{Value: "row1col1"},
-		&redshiftdataV2types.FieldMemberStringValue{Value: "row1col2"},
+		&redshiftdatatypes.FieldMemberStringValue{Value: "row1col1"},
+		&redshiftdatatypes.FieldMemberStringValue{Value: "row1col2"},
 	},
 	{
-		&redshiftdataV2types.FieldMemberStringValue{Value: "row2col1"},
-		&redshiftdataV2types.FieldMemberStringValue{Value: "row2col2"},
+		&redshiftdatatypes.FieldMemberStringValue{Value: "row2col1"},
+		&redshiftdatatypes.FieldMemberStringValue{Value: "row2col2"},
 	},
 }
 
@@ -41,18 +41,18 @@ type RedshiftService struct {
 
 // GetStatementResult returns a GetStatementResultOutput
 // When mockRedshiftService.calledTimesCountDown is more than 0, the GetStatementResultOutput will have a next token
-func (s *RedshiftService) GetStatementResult(_ context.Context, _ *redshiftdataV2.GetStatementResultInput, _ ...func(*redshiftdataV2.Options)) (*redshiftdataV2.GetStatementResultOutput, error) {
+func (s *RedshiftService) GetStatementResult(_ context.Context, _ *redshiftdata.GetStatementResultInput, _ ...func(*redshiftdata.Options)) (*redshiftdata.GetStatementResultOutput, error) {
 	s.CalledTimesCountDown--
 	s.CalledTimesCounter++
 
 	if s.CalledTimesCountDown == 0 {
-		return &redshiftdataV2.GetStatementResultOutput{
+		return &redshiftdata.GetStatementResultOutput{
 			ColumnMetadata: columnMetaData,
 			Records:        twoRecords,
 		}, nil
 	}
 
-	return &redshiftdataV2.GetStatementResultOutput{
+	return &redshiftdata.GetStatementResultOutput{
 		ColumnMetadata: columnMetaData,
 		Records:        twoRecords,
 		NextToken:      aws.String("nexttoken"),
@@ -63,21 +63,21 @@ const DESCRIBE_STATEMENT_FAILED = "DESCRIBE_STATEMENT_FAILED"
 
 // DescribeStatement returns a DescribeStatementOutput
 // When DescribeStatementInput.Id == DESCRIBE_STATEMENT_FAILED, an the output will include an error message that is equal to the input id
-// When DescribeStatementInput.Id == DESCRIBE_STATEMENT_FINISHED, the output will have a status redshiftdataV2.StatusStringFinished once mockRedshiftService.calledTimesCountDown == 0
-func (s *RedshiftService) DescribeStatement(_ context.Context, input *redshiftdataV2.DescribeStatementInput, _ ...func(options *redshiftdataV2.Options)) (*redshiftdataV2.DescribeStatementOutput, error) {
+// When DescribeStatementInput.Id == DESCRIBE_STATEMENT_FINISHED, the output will have a status redshiftdata.StatusStringFinished once mockRedshiftService.calledTimesCountDown == 0
+func (s *RedshiftService) DescribeStatement(_ context.Context, input *redshiftdata.DescribeStatementInput, _ ...func(options *redshiftdata.Options)) (*redshiftdata.DescribeStatementOutput, error) {
 	s.CalledTimesCountDown--
 	s.CalledTimesCounter++
 
-	output := &redshiftdataV2.DescribeStatementOutput{}
+	output := &redshiftdata.DescribeStatementOutput{}
 	if s.CalledTimesCountDown == 0 {
 		if *input.Id == DESCRIBE_STATEMENT_FAILED {
-			output.Status = redshiftdataV2types.StatusStringFailed
+			output.Status = redshiftdatatypes.StatusStringFailed
 			output.Error = aws.String(DESCRIBE_STATEMENT_FAILED)
 		} else {
-			output.Status = redshiftdataV2types.StatusStringFinished
+			output.Status = redshiftdatatypes.StatusStringFinished
 		}
 	} else {
-		output.Status = redshiftdataV2types.StatusStringStarted
+		output.Status = redshiftdatatypes.StatusStringStarted
 	}
 	return output, nil
 }
