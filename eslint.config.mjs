9faa950c13ae5116e00378f74018913ff500183a
Ida Štambuk <ida.stambuk@grafana.com>
2025-06-17 17:32:55 +0200

    Migrate to eslint v9 (#408)

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    ignores: ['**/node_modules', '**/dist'],
+  },
+  {
+    extends: compat.extends('./.config/.eslintrc'),
+  },
+  {
+    rules: {
+      'deprecation/deprecation': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.{ts,tsx,js,jsx}'],
+    rules: {
+      '@typescript-eslint/no-deprecated': 'warn',
+    },
+  },
+]);
