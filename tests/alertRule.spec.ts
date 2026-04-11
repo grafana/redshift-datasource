b9789ab5034cbad9f4195e2f03d016d09729ebc8
Kevin Yu <kevinwcyu@users.noreply.github.com>
2026-04-10 21:19:54 -0700

    revert

--- tests/alertRule.spec.ts
+++ tests/alertRule.spec.ts
@@ -13,30 +13,10 @@ test('should successfully create an alert rule', async ({
   selectors,
 }) => {
   const ds = await readProvisionedDataSource({ fileName: 'aws-redshift-e2e.yaml', name: 'AWS Redshift E2E' });
-
-  const legacyQueryEditorRow = alertRuleEditPage
-    .getByGrafanaSelector(selectors.components.QueryEditorRows.rows)
-    .filter({
-      has: alertRuleEditPage.getByGrafanaSelector(selectors.components.QueryEditorRow.title('A')),
-    });
-  // TODO: Remove this fallback once @grafana/plugin-e2e picks up the Grafana 13 query-row selectors.
-  const queryEditorRow =
-    (await legacyQueryEditorRow.count()) > 0
-      ? legacyQueryEditorRow
-      : alertRuleEditPage.getByGrafanaSelector('data-testid Query editor row').filter({
-          has: alertRuleEditPage.getByGrafanaSelector('data-testid Query editor row title A'),
-        });
-
-  await expect(queryEditorRow.getByTestId(selectors.components.DataSourcePicker.inputV2)).toBeVisible();
-  await queryEditorRow.getByTestId(selectors.components.DataSourcePicker.inputV2).fill(ds.name);
-  await page.keyboard.press('ArrowDown');
-  await page.keyboard.press('ArrowUp');
-  await page.keyboard.press('Enter');
-
+  const queryA = alertRuleEditPage.getAlertRuleQueryRow('A');
+  await queryA.datasource.set(ds.name);
   await page.waitForFunction(() => window.monaco);
-  await alertRuleEditPage
-    .getByGrafanaSelector(selectors.components.CodeEditor.container, { root: queryEditorRow })
-    .click();
+  await queryA.getByGrafanaSelector(selectors.components.CodeEditor.container).click();
   await page.keyboard.insertText('SELECT environment, temperature FROM public.long_format_example limit 2');
   await expect(alertRuleEditPage.evaluate()).toBeOK();
 });
