5a4034d126c3460ea1a6e18008f4ea174a542531
Matt Vella <matt.vella@grafana.com>
2026-07-08 15:09:35 -0500

    Updated e2e tests

--- tests/variableQueryEditor.spec.ts
+++ tests/variableQueryEditor.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@grafana/plugin-e2e';
+import { gte } from 'semver';
 
-test('should successfully create a variable', async ({ variableEditPage, page, selectors }) => {
+test('should successfully create a variable', async ({ variableEditPage, page, selectors, grafanaVersion }) => {
   await variableEditPage.datasource.set('AWS Redshift E2E');
   await page.waitForFunction(() => window.monaco);
   const editor = page.getByTestId(selectors.components.CodeEditor.container);
@@ -9,6 +10,11 @@ test('should successfully create a variable', async ({ variableEditPage, page, s
   const queryDataRequest = variableEditPage.waitForQueryDataRequest();
   await variableEditPage.runQuery();
   await queryDataRequest;
+  if (gte(grafanaVersion, '13.0.0')) {
+    await variableEditPage
+      .getByGrafanaSelector(selectors.pages.Dashboard.Settings.Variables.Edit.QueryVariable.previewButton)
+      .click();
+  }
   await expect(variableEditPage).toDisplayPreviews(
     ['Classical', 'Jazz', 'MLB', 'MLS', 'Musicals', 'NBA', 'NFL', 'NHL', 'Opera', 'Plays', 'Pop'],
     { timeout: 15_000 }
